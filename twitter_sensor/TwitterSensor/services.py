885ad93e95823c0dbfc87105a5d9c959bddb089a
Alexander Arshavski <alex@Macintosh-4.local>
2009-05-17 20:35:37 +0300

    new file:   twitter_sensor/TwitterSensor/__init__.py 	new file:   twitter_sensor/TwitterSensor/models.py 	new file:   twitter_sensor/TwitterSensor/services.py 	new file:   twitter_sensor/TwitterSensor/tests.py 	new file:   twitter_sensor/__init__.py 	new file:   twitter_sensor/manage.py 	new file:   twitter_sensor/settings.py 	new file:   twitter_sensor/urls.py

---
+++ twitter_sensor/TwitterSensor/services.py
@@ -0,0 +1,48 @@
+from django.http import HttpResponse
+from models import *
+import twitter
+
+#def get_tweets(request, since_id, user, password):
+def get_tweets(request):
+    users = []
+    stories = []
+    tweets = []
+    if request.GET:
+        if 'user' in request.GET and 'password' not in request.GET:
+            return HttpResponse('Please enter password for %s' % request.GET['user'])
+        if 'user' not in request.GET:
+            return HttpResponse('Please enter twitter username')
+    else:
+        return HttpResponse('Please enter twitter username')
+    try:
+        client = twitter.Api(username=request.GET['user'], password=request.GET['password'])
+        latest_posts = client.GetFriendsTimeline(request.GET['user'])
+    except:
+        return HttpResponse('Authentication failed. Check your username and password')
+    for post in latest_posts:
+        reply_to_user = ''
+        if post.text.startswith("@"):
+            reply_to_user = post.text[1:].split(" ")[0]
+        story = Story()
+        story.text = post.text
+        story.user_external_id = post.user.id
+        story.external_id = post.id
+        story.external_url = ''
+        story.in_reply_to_user = reply_to_user
+        story.last_update = post.created_at
+
+        user = User()
+        user.external_id = post.user.id
+        user.name = post.user.name
+        user.screen_name = post.user.screen_name
+        user.url = post.user.url
+        user.location = ''
+        user.description = post.user.description
+        user.image = post.user.profile_image_url
+
+        users.append(user.name)
+        stories.append(story.text)
+
+    tweets = [users, stories]
+
+    return HttpResponse(tweets)
