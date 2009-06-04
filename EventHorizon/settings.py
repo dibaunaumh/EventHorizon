e94ec2cfb01fd3f98eb5f2667abeaee90b629791
Udi h Bauman <dibaunaumh@gmail.com>
2009-06-04 20:12:38 +0300

    Fixed db path setting & added initial fixtures

--- EventHorizon/settings.py
+++ EventHorizon/settings.py
@@ -9,8 +9,12 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
+import os 
+
+base_dir = os.path.abspath(os.path.dirname(__file__))
+
 DATABASE_ENGINE = 'sqlite3'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = '/Users/udibauman/Documents/projects/EventHorizon/EventHorizon/db/EventHorizon.db'             # Or path to database file if using sqlite3.
+DATABASE_NAME = '%s/db/EventHorizon.db' % base_dir            # Or path to database file if using sqlite3.
 DATABASE_USER = ''             # Not used with sqlite3.
 DATABASE_PASSWORD = ''         # Not used with sqlite3.
 DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
