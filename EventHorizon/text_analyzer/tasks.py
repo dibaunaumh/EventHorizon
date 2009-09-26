b5e7aa3b3b7b121ba9a80a25d746b51bdb9d1662
alexarsh <alexarsh5@gmail.com>
2009-09-26 13:37:20 +0300

    new file:   tasks.py

---
+++ EventHorizon/text_analyzer/tasks.py
@@ -0,0 +1,17 @@
+from services import *
+
+class ExtractNamedEntitiesTask(Task):
+     name = "text_analyzer.extract_named_entities"
+     def run(self, request, **kwargs):
+         logger = self.get_logger(**kwargs)
+         logger.info("Extracting named entities...")
+         return extract_named_entities(request)
+tasks.register(ExtractNamedEntitiesTask)
+
+class ExtractEssenceTask(Task):
+     name = "text_analyzer.extract_essence"
+     def run(self, request, **kwargs):
+         logger = self.get_logger(**kwargs)
+         logger.info("Extracting essence...")
+         return extract_essence(request)
+tasks.register(ExtractEssenceTask)
